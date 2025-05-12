c6b923e41f8e9ea23c5be7a13ccfa9dc0f2c1510
NeanderDanubio26827 <neander.eng@alu.ufc.br>
2025-05-11 21:43:58 -0300

    feat: local build have been success

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import dotenv from 'dotenv';
+
 dotenv.config();
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: './vitest-setup.js',
+        setupFiles: './vitest-setup.ts',
         include: [
             './src/tests/unit/**/*.spec.tsx',
             './src/tests/integration/**/*.spec.tsx',
