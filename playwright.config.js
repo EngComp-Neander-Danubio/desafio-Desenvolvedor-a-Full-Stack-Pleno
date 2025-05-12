59690ccf530ab8690bbd873ea7fc5b9bf7596102
NeanderDanubio26827 <neander.eng@alu.ufc.br>
2025-05-11 22:21:45 -0300

    fix: enviroment variables was changing

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     testDir: './src/tests/e2e',
     timeout: 30 * 1000,
     use: {
-        baseURL: import.meta.env.VITE_API_URL,
+        baseURL: import.meta.env.API_URL,
         headless: true,
         viewport: { width: 1280, height: 720 },
     },
